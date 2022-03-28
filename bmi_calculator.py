83bdbcecdd5b90822c196c1379a4a6dcb1a9737c
Nduduzo Ntshangase <nduduzontshangase@gmail.com>
2022-03-28 13:09:26 +0200

    add print() and edit round function

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -6,4 +6,6 @@ height = float(input("Enter your height in meters: "))
 weight = float(input("Enter your weight in kg's: "))
 
 bmi = weight / height **2
-bmi_t = round(bmi, 1)
+bmi_t = round(bmi)
+
+print(f"Your bmi is {bmi_t}.")
