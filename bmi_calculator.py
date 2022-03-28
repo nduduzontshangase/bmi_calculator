1726d9fac69059426e46a2c143604873b4b07881
Nduduzo Ntshangase <nduduzontshangase@gmail.com>
2022-03-28 13:12:48 +0200

    add if statements

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -8,4 +8,13 @@ weight = float(input("Enter your weight in kg's: "))
 bmi = weight / height **2
 bmi_t = round(bmi)
 
-print(f"Your bmi is {bmi_t}.")
+if bmi_t <= 18.5:
+    print(f"Your BMI is {bmi_t}, you are underweight.")
+elif bmi_t <= 25:
+    print(f"Your BMI is {round(bmi_t)}, you have a normal weight.")
+elif bmi_t <= 30:
+    print(f"Your BMI is {round(bmi_t)}, you are slightly overweight.")
+elif bmi_t <=35:
+    print(f"Your BMI is {round(bmi_t)}, you are obese.")
+else:
+    print(f"Your BMI is {round(bmi_t)}, you are clinically obese.")
